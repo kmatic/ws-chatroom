095801ca379f9e9e014e270c2b9afb582bad5945
Kristopher <kristopher_matic@hotmail.com>
2023-02-01 00:06:40 -0800

    finish message events and basic styling

--- src/App.js
+++ src/App.js
@@ -2,103 +2,160 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import styled from "styled-components";
+import moment from "moment";
 
 const username = prompt("What is your username");
 
 const socket = io("ws://localhost:5000");
 
 const StyledDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  color: white;
-
-  & > div {
     display: flex;
-    gap: 20px;
-  }
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    color: white;
+
+    & > div {
+        display: flex;
+        gap: 20px;
+    }
 `;
 
 const ChatBox = styled.div`
-  background-color: rgb(51 65 85);
-  padding: 20px;
-  width: 600px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
+    background-color: rgb(51 65 85);
+    padding: 20px;
+    width: 600px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
 
-  input {
-    width: 50%;
-    padding: 6px;
-  }
+    input {
+        width: 50%;
+        padding: 6px;
+    }
 
-  button {
-    padding: 6px 15px;
-  }
+    button {
+        padding: 6px 15px;
+    }
 `;
 
 const UserBox = styled.div`
-  background-color: rgb(51 65 85);
-  padding: 20px;
-  width: 200px;
+    background-color: rgb(51 65 85);
+    padding: 20px;
+    width: 200px;
+
+    ul {
+        list-style: none;
+        padding: 0;
+
+        li {
+            margin: 10px 0px;
+        }
+    }
 `;
 
 const Messages = styled.div`
-  background-color: lightgrey;
-  width: 100%;bob
+    background-color: lightgrey;
+    width: 100%;
+    height: 400px;
+    overflow-y: scroll;
+    color: black;
 
-  height: 400px;
+    > div {
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        > div {
+            display: flex;
+            gap: 20px;
+        }
+    }
+
+    p {
+        margin: 10px 0px;
+    }
 `;
 
 const App = () => {
-  const [messages, SetMessages] = useState([]);
-  const [message, setMessage] = useState("");
-  const [users, setUsers] = useState([]);
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState("");
+    const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log("connected", socket.id);
-      socket.emit("username-provided", username);
-    });
+    const submitMessage = (e) => {
+        e.preventDefault();
+        socket.emit("message-sent", message);
+        setMessage("");
+    };
 
-    socket.on("connected", (users) => {
-      setUsers(users);
-    });
+    useEffect(() => {
+        socket.on("connect", () => {
+            console.log("connected", socket.id);
+            socket.emit("username-provided", username);
+        });
 
-    socket.on("disconnected", (id) => {
-      setUsers((users) => {
-        return users.filter((user) => user.id !== id);
-      });
-    });
-  }, []);
+        socket.on("connected", (users) => {
+            setUsers(users);
+        });
 
-  return (
-    <StyledDiv>
-      <div>
-        <ChatBox>
-          <h1>Messages</h1>
-          <Messages></Messages>
-          <form>
-            <input
-              type="text"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <button>Send</button>
-          </form>
-        </ChatBox>
-        <UserBox>
-          <h1>Users</h1>
-          <div>
-            <ul>
-              {users && users.map((user) => <li key={user.id}>{user.name}</li>)}
-            </ul>
-          </div>
-        </UserBox>
-      </div>
-    </StyledDiv>
-  );
+        socket.on("disconnected", (id) => {
+            setUsers((users) => {
+                return users.filter((user) => user.id !== id);
+            });
+        });
+
+        socket.on("message-received", (message) => {
+            setMessages((messages) => {
+                return [...messages, message];
+            });
+        });
+    }, []);
+
+    return (
+        <StyledDiv>
+            <div>
+                <ChatBox>
+                    <h1>Messages</h1>
+                    <Messages>
+                        <div>
+                            {messages &&
+                                messages.map((message, idx) => (
+                                    <div key={idx}>
+                                        <p>
+                                            {moment(message.date).format(
+                                                "h:mm:ss a"
+                                            )}
+                                        </p>
+                                        <p>{message.user.name}</p>
+                                        <p>{message.message}</p>
+                                    </div>
+                                ))}
+                        </div>
+                    </Messages>
+                    <form onSubmit={(e) => submitMessage(e)}>
+                        <input
+                            type="text"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                        <button>Send</button>
+                    </form>
+                </ChatBox>
+                <UserBox>
+                    <h1>Users</h1>
+                    <div>
+                        <ul>
+                            {users &&
+                                users.map((user) => (
+                                    <li key={user.id}>{user.name}</li>
+                                ))}
+                        </ul>
+                    </div>
+                </UserBox>
+            </div>
+        </StyledDiv>
+    );
 };
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -5,28 +5,38 @@ const { Server } = require("socket.io");
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:1234",
-  },
+    cors: {
+        origin: "http://localhost:1234",
+    },
 });
 
 const users = {};
 
 io.on("connection", (socket) => {
-  socket.on("username-provided", (username) => {
-    const user = {
-      id: socket.id,
-      name: username,
-    };
-    users[socket.id] = user;
-    // io.emit("connected", user);
-    io.emit("connected", Object.values(users));
-  });
+    socket.on("username-provided", (username) => {
+        const user = {
+            id: socket.id,
+            name: username,
+        };
 
-  socket.on("disconnect", () => {
-    delete users[socket.id];
-    io.emit("disconnected", socket.id);
-  });
+        users[socket.id] = user;
+        io.emit("connected", Object.values(users));
+    });
+
+    socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("disconnected", socket.id);
+    });
+
+    socket.on("message-sent", (message) => {
+        const msg = {
+            message: message,
+            user: users[socket.id],
+            date: new Date().toISOString(),
+        };
+
+        io.emit("message-received", msg);
+    });
 });
 
 httpServer.listen(5000);
